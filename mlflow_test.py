72a9adf26256947b1ee24c5e1d1cc6f43957262e
Badal Kumar Sahoo <badalkumarsahoo.2001@gmail.com>
2026-04-16 23:13:55 +0530

    Initial ML project with MLflow integration

--- mlflow_test.py
+++ mlflow_test.py
@@ -1,8 +1,8 @@
-#End to End Data Science Project"# mlproject" 
 import dagshub
 dagshub.init(repo_owner='BadalKumarSahoo20', repo_name='mlproject', mlflow=True)
 
 import mlflow
+
 with mlflow.start_run():
-  mlflow.log_param('parameter name', 'value')
-  mlflow.log_metric('metric name', 1)
+    mlflow.log_param('parameter name', 'value')
+    mlflow.log_metric('metric name', 1)
