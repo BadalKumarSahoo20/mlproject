7fc69de60c182b998090ac2a9ae70d648b55b491
Badal Kumar Sahoo <badalkumarsahoo.2001@gmail.com>
2026-04-16 21:40:22 +0530

    data transformation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.mlproject.exception import CustomException
 import sys
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
+from src.mlproject.components.data_transformation import Data_TransformationConfig,Data_Transformation
 
 
 if __name__=="__main__":
@@ -12,7 +13,10 @@ if __name__=="__main__":
     try:
         # data_ingestion_config=DataIngestionConfig
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        #data_transformation_config=Data_TransformationConfig()
+        data_transformation = Data_Transformation()
+        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
         
     except Exception as e:
         logging.info("Custom Exception")
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -6,38 +6,46 @@ import pandas as pd
 import numpy as np
 from dotenv import load_dotenv
 import pymysql
-
-
-
-
+import pickle
 
 load_dotenv()
-host=os.getenv("host")
-user=os.getenv("user")
-password=os.getenv("password")
-db=os.getenv('db')
 
-
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv('db')
 
 
 def read_sql_data():
     logging.info("Reading SQL database Started")
     try:
-        mydb=pymysql.connect(
+        mydb = pymysql.connect(
             host=host,
             user=user,
             password="Badalkumar@2001",
             db=db
         )
-        logging.info(f"Connection Established{mydb}")
-        df=pd.read_sql_query('Select * from student',mydb)
+
+        logging.info(f"Connection Established {mydb}")
+
+        df = pd.read_sql_query('Select * from student', mydb)
         print(df.head())
-        
-        
+
         return df
-        
-        
-        
-        
+
     except Exception as ex:
-        raise CustomException(ex,sys)
+        raise CustomException(ex, sys)
+
+
+# ✅ NOW OUTSIDE (Correct)
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
